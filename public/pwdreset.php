1440790b49cf689139c7799d12c3864f8b485a23
naomipham <69292170+naomipham@users.noreply.github.com>
2021-08-31 15:57:11 +1000

    final commit

--- public/pwdreset.php
+++ public/pwdreset.php
@@ -77,14 +77,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8">
     <title>Reset Password</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/spectre.css">
+    <link rel="stylesheet" href="assets/css/spectre-exp.css">
+    <link rel="stylesheet" href="assets/css/spectre-icons.css">
     <style type="text/css">
         body{ font: 14px sans-serif; }
         .wrapper{ width: 350px; padding: 20px; }
     </style>
 </head>
 <body>
-    <div class="wrapper">
-        <h2>Reset Password</h2>
+    <div class="location">
+        <h1>Reset Password</h1>
         <p>Please fill out this form to reset your password.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
             <div class="form-group <?php echo (!empty($new_password_err)) ? 'has-error' : ''; ?>">
@@ -97,9 +101,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <input type="password" name="confirm_password" class="form-control">
                 <span class="help-block"><?php echo $confirm_password_err; ?></span>
             </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <a class="btn btn-link" href="index.php">Cancel</a>
+            <div class="button">
+                <input type="submit" class="button" value="Submit">
+            </div>
+            <div class="button">
+                <a class="button" href="index.php">Cancel</a>
             </div>
         </form>
     </div>    
